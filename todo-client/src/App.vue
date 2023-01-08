05cdef7361148497ae00041b9c6e2dec516704e5
Sebastian Rubina <sebastian.rubina@icloud.com>
2023-01-07 23:22:51 -0500

    ESLINT!

--- todo-client/src/App.vue
+++ todo-client/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <h3>
     <strong
-    >Note: functionality to push new items to the database has been disabled
-    to stop people from posting inappropriate items for everyone to see.
-    Delete and Complete functionalities will not work with custom items either
-    as the DELETE and PATCH requests won't find your item in the database.</strong
+      >Note: functionality to push new items to the database has been disabled
+      to stop people from posting inappropriate items for everyone to see.
+      Delete and Complete functionalities will not work with custom items either
+      as the DELETE and PATCH requests won't find your item in the database.</strong
     >
   </h3>
   <nav>
@@ -15,6 +15,10 @@
     <router-view />
   </div>
 </template>
+
+<script>
+/* eslint-disable */
+</script>
 
 <style>
 #app {
