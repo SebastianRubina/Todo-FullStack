2de8f72841bf7e9430cb316ae0c77a04c1f59e69
Sebastian Rubina <sebastian.rubina@icloud.com>
2023-01-07 23:40:02 -0500

    Fallback Items

--- todo-client/src/views/TodoList.vue
+++ todo-client/src/views/TodoList.vue
@@ -48,6 +48,18 @@ export default {
         .delete(`https://todofullstack.azurewebsites.net/todoitem/${id}`)
         .then(() => this.getItems())
         .catch((err) => console.log(err));
+
+      let date = new Date()
+      let newItem = {
+        title: "New Item!"
+        description: "To prevent from running out of items!"
+        date: `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`,
+        time: `${date.getHours}:${date.getMinutes}:${date.getSeconds}`,
+      };
+      axios
+        .post("https://todofullstack.azurewebsites.net/todoitem", newItem)
+        .then((this.id = `00000000-${Math.floor(Math.random()) * 9}${Math.floor(Math.random()) * 9}${Math.floor(Math.random()) * 9}${Math.floor(Math.random()) * 9}-0000-0000-000000000000`))
+        .catch((err) => console.log(err));
     },
     updateItem(id) {
       axios
