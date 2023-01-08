e181d116e85afa9060d0bc62fb8d2fed4f1f26dd
Sebastian Rubina <sebastian.rubina@icloud.com>
2023-01-07 23:03:51 -0500

    Test

--- todo-client/src/views/AddItem.vue
+++ todo-client/src/views/AddItem.vue
@@ -6,6 +6,7 @@
 
 <script>
 import AddForm from "@/components/AddForm.vue";
+import TodoItems from "@/views/TodoList.vue;"
 import axios from "axios";
 
 export default {
@@ -15,10 +16,11 @@ export default {
   },
   methods: {
     addItem(item) {
-      axios
-        .post("https://todofullstack.azurewebsites.net/todoitem", item)
-        .then((this.id = "00000000-0000-0000-0000-000000000000"))
-        .catch((err) => console.log(err));
+      // axios
+      //   .post("https://todofullstack.azurewebsites.net/todoitem", item)
+      //   .then((this.id = "00000000-0000-0000-0000-000000000000"))
+      //   .catch((err) => console.log(err));
+      TodoItems.push(item);
     },
   },
 };
